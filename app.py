92ff92d6e6b2072f0a7818fa8302bd8438dcd63f
Samubarboza <samu_junior95@hotmail.com>
2026-01-13 16:36:56 -0300

    .

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, redirect
+import requests
+import os
+
+from models import db
+from models import Favorite
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DB_PATH = os.path.join(BASE_DIR, "instance", "app.db")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+API_URL = "https://rickandmortyapi.com/api/character"
+
+# ruta principal
+@app.route("/")
+def index():
+    page = request.args.get("page", 1)
+    # aca comienza la logica de la busqueda de personajes. si hay tiempo agregamos
+    name = request.args.get("name")
+
+    # Si hay búsqueda → no paginamos
+    if name:
+        response = requests.get(API_URL, params={"name": name})
+        if response.status_code != 200:
+            return render_template("index.html", characters=[], search=True, error_message="Personaje no encontrado")
+        data = response.json()
+        return render_template("index.html", characters=data["results"], search=True)
+
+    # Listado normal con paginación
+    response = requests.get(API_URL, params={"page": page})
+    data = response.json()
+
+    return render_template("index.html", characters=data["results"], info=data["info"], page=int(page), search=False)
+
+
+@app.route("/save", methods=["POST"])
+def save():
+    api_id = request.form["api_id"]
+    name = request.form["name"]
+    image = request.form["image"]
+
+    if not Favorite.query.filter_by(api_id=api_id).first():
+        fav = Favorite(api_id=api_id, name=name, image=image)
+        db.session.add(fav)
+        db.session.commit()
+
+    return redirect("/")
+
+# ruta de los peronajes favoritos que se guardan en la base de datios
+@app.route("/favorites")
+def favorites():
+    favorites = Favorite.query.all()
+    return render_template("favorites.html", favorites=favorites)
+
+
+# ruta eliminar personajes
+@app.route("/delete/<int:id>", methods=["POST"])
+def delete(id):
+    fav = Favorite.query.get(id)
+    if fav:
+        db.session.delete(fav)
+        db.session.commit()
+    return redirect("/favorites")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    api_id = db.Column(db.Integer, unique=True)
+    name = db.Column(db.String(100))
+    image = db.Column(db.String(255))
